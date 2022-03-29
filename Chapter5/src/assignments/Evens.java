fd8ee0defe32ca367264192c223378fcce4a0819
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-03-29 11:59:10 -0600

    working on chapter 5

---
+++ Chapter5/src/assignments/Evens.java
@@ -0,0 +1,42 @@
+/*
+
+Program: digits.java          Last Date of this Revision: September 30, 2019
+
+Purpose: 
+
+Author: Rohaan 
+School: CHHS
+Course: Computer science 20
+ 
+
+*/
+package assignments;
+
+import java.util.Scanner;
+
+public class Evens {
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);	
+		System.out.println("");
+		
+		
+		 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
+/* Screen Dump
+
+
+ */
