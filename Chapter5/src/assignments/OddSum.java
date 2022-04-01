28b8b92a81666a15a406cd22c517c6ebaba3f6e8
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-04-01 09:28:45 -0600

    working on this

---
+++ Chapter5/src/assignments/OddSum.java
@@ -0,0 +1,36 @@
+/*
+
+Program: digits.java          Last Date of this Revision: September 30, 2019
+
+Purpose: 
+
+Author: Rohaan 
+School: CHHS
+Course: Computer science 20
+ 
+
+*/
+package assignments;
+
+
+
+public class OddSum {
+	public static void main(String[] args) 
+	{
+		
+for (int i = 2; i <= 20; i++) {
+	System.out.println(i++);
+}
+		
+		 
+		
+		
+		
+	}
+
+}
+/* Screen Dump
+
+
+
+ */
