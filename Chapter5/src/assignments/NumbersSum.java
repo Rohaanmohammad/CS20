234c3a99113aca7fa49f4c198257fa01a702d6d7
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-04-01 09:26:13 -0600

    done

--- Chapter5/src/assignments/NumbersSum.java
+++ Chapter5/src/assignments/NumbersSum.java
@@ -8,7 +8,7 @@ Author: Rohaan
 School: CHHS
 Course: Computer science 20
  
-
+done
 */
 package assignments;
 
@@ -19,26 +19,23 @@ public class NumbersSum {
 	{
 	
 		Scanner input = new Scanner(System.in);	
-	//System.out.println("enter number");
-		
-	//int userinput = input.nextInt();
-
-	//for (int i = 2; i <= 20; i++) {
-		//	System.out.println(i);
-		//}
 			
 		int num;
 		int sum = 0;
+		
 		System.out.println("enter number");
+		
 		num = input.nextInt();
-		for (; num <= 10; num++) {
+		
+		for (; num >= 1; num--) {
 			System.out.println(num);
 			sum = sum + num;
 		}
-		System.out.println(sum);
+		
+		System.out.println("the sum of all the number is: "+sum);
 		
 		
-		
+	
 		
 		
 		
@@ -48,6 +45,28 @@ public class NumbersSum {
 
 }
 /* Screen Dump
-
+enter number
+20
+20
+19
+18
+17
+16
+15
+14
+13
+12
+11
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+the sum of all the number is: 210
 
  */
