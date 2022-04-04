b80885ff3ffdd6abc73291d260294ead58e2461d
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-04-04 09:28:08 -0600

    done

--- Chapter5/src/assignments/OddSum.java
+++ Chapter5/src/assignments/OddSum.java
@@ -12,11 +12,26 @@ Course: Computer science 20
 */
 package assignments;
 
-
+import java.util.Scanner;
 
 public class OddSum {
 	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);	
+		
+		int num;
+		int sum = 0;
+		
+		System.out.println("enter number");
+		
+		num = input.nextInt();
+		
+		for (int i = 2; num >= 1; num--) {
+			System.out.println(num--);
+			sum = sum + num;
+		}
+		
+		System.out.println("the sum of all the number is: "+sum);
 		
 
 		
@@ -24,10 +39,21 @@ public class OddSum {
 		
 		
 		
+		
+		
 	}
 
 }
 /* Screen Dump
+enter number
+11
+11
+9
+7
+5
+3
+1
+the sum of all the number is: 30
 
 
 
