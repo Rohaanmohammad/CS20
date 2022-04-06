50ee4edcaf7b36656ab1b4c5f9a33f084377c2e7
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-04-06 09:04:06 -0600

    done

--- Chapter5/src/assignments/AccountSetup.java
+++ Chapter5/src/assignments/AccountSetup.java
@@ -26,16 +26,15 @@ public class AccountSetup {
 		
 		System.out.println(username);
 		
+		
+		
 		System.out.println("enter a password that is at least 8 characters: ");
 		
 		String password = input.nextLine();
 		
 		System.out.println(password);
 		
-		System.out.println("tour name is " + username + " and your password is " +  password + ".");
-		
-		
-		
+		System.out.println("your name is " + username + " and your password is " +  password + ".");
 		
 		
 		
@@ -48,6 +47,13 @@ public class AccountSetup {
 
 }
 /* Screen Dump
+enter a username: 
+MattD
+MattD
+enter a password that is at least 8 characters: 
+Programmer7
+Programmer7
+your name is MattD and your password is Programmer7.
 
 
  */
