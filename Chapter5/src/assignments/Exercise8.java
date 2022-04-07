c03b86bf994c5c4e4a0c28bcc24c443ca239f1ff
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-04-07 11:49:47 -0600

    working on this

---
+++ Chapter5/src/assignments/Exercise8.java
@@ -0,0 +1,34 @@
+/*
+
+Program: digits.java          Last Date of this Revision: September 30, 2019
+
+Purpose: 
+
+Author: Rohaan 
+School: CHHS
+Course: Computer science 20
+ 
+done
+*/
+package assignments;
+
+import java.util.Scanner;
+
+public class Exercise8 {
+	public static void main(String[] args) 
+	{
+	
+		Scanner input = new Scanner(System.in);	
+		
+	
+		
+		
+		
+	}
+
+}
+/* Screen Dump
+
+
+
+ */
