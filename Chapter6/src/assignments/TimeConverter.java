57015768d31f142f0241a871b782760eaaca3335
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-04-11 10:22:27 -0600

    working

--- Chapter6/src/assignments/TimeConverter.java
+++ Chapter6/src/assignments/TimeConverter.java
@@ -30,4 +30,88 @@ public static void hourstominutes() {
 	System.out.println("minutes is: " + mins);
 }
 
+public static void daystohours() {
+
+	double hours, days;
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enter number of days: ");
+	
+	days = input.nextDouble();
+	
+	hours = days * 24;
+	
+	System.out.println("hours is: " + hours);
+}
+
+public static void minutestohours() {
+
+	double hours, mins;
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enter number of minutes: ");
+	
+	mins = input.nextDouble();
+	
+	hours = mins / 60;
+	
+	System.out.println("hours is: " + hours);
+}
+
+public static void hourstodays() {
+
+	double hours, days;
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enter number of hours: ");
+	
+	hours = input.nextDouble();
+	
+	days = hours * 2;
+	
+	System.out.println("number of days is: " + days);
+}
+
+public static void main(String[] args) {
+	
+	int choose;
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("1. hours to minutes coversion");
+	
+	System.out.println("2. days to hours converion");
+	
+	System.out.println("3. minutes to hours conversion");
+	
+	System.out.println("4. hours to days conversion");
+	
+	choose = input.nextInt();
+	
+	if (choose == 1 ) {
+		hourstominutes();
+	}
+	else {
+		daystohours();
+	}
+	if (choose == 3){
+		minutestohours();
+	}
+	else {
+		hourstodays();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
 }
