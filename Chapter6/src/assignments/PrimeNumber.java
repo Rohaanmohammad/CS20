478dde87ff64cb9707fae090ec74d8ee1727c9c0
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-04-14 11:20:17 -0600

    done

--- Chapter6/src/assignments/PrimeNumber.java
+++ Chapter6/src/assignments/PrimeNumber.java
@@ -1,5 +1,45 @@
+/*
+
+Program: digits.java          Last Date of this Revision: September 30, 2019
+
+Purpose: 
+
+Author: Rohaan 
+School: CHHS
+Course: Computer science 20
+ 
+done
+*/
 package assignments;
 
-public class PrimeNumber {
+import java.util.Scanner;
 
-} 
+public class PrimeNumber {
+	
+		 public static void main(String args[]){    
+			 
+			 Scanner input = new Scanner(System.in);
+			 System.out.println("enter number");
+			 int num = input.nextInt();
+			    boolean num1 = false;
+			    for (int i = 2; i <= num / 2; ++i) {
+			     
+			      if (num % i == 0) {
+			        num1 = true;
+			        break;
+			      }
+			    }
+
+			    if (!num1)
+			    	System.out.println(num + " is a prime number.");
+			    else
+			      System.out.println(num + " is not a prime number.");
+			  }
+			
+			}    
+
+/* Screen Dump
+
+
+
+ */
