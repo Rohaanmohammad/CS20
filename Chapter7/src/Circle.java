e71ebd19dbc757fda0ae33cb4dae937bd3e2a1bf
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-04-22 09:44:55 -0600

    working

--- Chapter7/src/Circle.java
+++ Chapter7/src/Circle.java
@@ -1,4 +1,22 @@
+/*
+
+Program: digits.java          Last Date of this Revision: September 30, 2019
+
+Purpose: 
+
+Author: Rohaan 
+School: CHHS
+Course: Computer science 20
+ 
+
+*/
+
+import java.util.Scanner;
 
 public class Circle {
-
+	public static void main(String[] args) {
+		Circle spot = new Circle();
+		
+		
+	}
 }
