a1022d2b709a684a9618eb25f2e7131d15ceccf6
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-04-25 10:23:34 -0600

    working

--- Chapter7/src/Circle.java
+++ Chapter7/src/Circle.java
@@ -14,9 +14,29 @@ Course: Computer science 20
 import java.util.Scanner;
 
 public class Circle {
+	
+	public static void circumference() {
+		Scanner input = new Scanner(System.in);
+		
+		int userinput = input.nextInt();
+		double num = 2;
+		double pi = 3.14;
+		double radius = userinput;
+		double circumference;
+		
+		circumference = num * pi * radius;
+		System.out.println("gg: " + circumference);	
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
 		Circle spot = new Circle();
 		
+		spot.setRadius(3);
 		
+		System.out.println("Circle radius: " + spot.getRadius());	
+		System.out.println("Circle circumference: " + spot.circumference() );	
 	}
 }
