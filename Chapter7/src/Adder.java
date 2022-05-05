b618a884b4e5e9c50be4df214d748bce0a8c9969
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-05-05 11:31:24 -0600

    working

---
+++ Chapter7/src/Adder.java
@@ -0,0 +1,29 @@
+/*
+
+Program: digits.java          Last Date of this Revision: September 30, 2019
+
+Purpose: 
+
+Author: Rohaan 
+School: CHHS
+Course: Computer science 20
+ 
+
+*/
+
+import java.util.Scanner;
+
+public class Adder {
+	public static void main(String[] args) 
+	{
+		
+		Scanner input = new Scanner(System.in);
+		
+		int userinput = input.nextInt(); 
+		 
+		 
+		 
+		
+		
+	}
+}
