193919909784d4808fccd59e080f9b68ca49d4f1
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-05-17 11:08:56 -0600

    done

--- Chapter9/src/assignments/Squares.java
+++ Chapter9/src/assignments/Squares.java
@@ -1,13 +1,23 @@
 package assignments;
 
+
+
 public class Squares {
 	public static void main(String[] args) 
 	{
 		
 		
 		
-		
-		
+		int[] square = {1, 2, 3, 4, 5};
+
+
+	    for (int i = 0; i < square.length; i++) {
+	        square[i] = (int) Math.pow(square[i], 2);
+	    }
+
+	    for (int num : square) {
+	        System.out.println(num);
+	    }
 		
 		
 		
