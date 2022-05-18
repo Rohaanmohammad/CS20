999804d4b5c9f179013551a9bdf9ef930ffe1076
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-05-18 09:21:41 -0600

    done

--- Chapter9/src/assignments/Reverse.java
+++ Chapter9/src/assignments/Reverse.java
@@ -3,9 +3,15 @@ package assignments;
 public class Reverse {
 	public static void main(String[] args) 
 	{
+		System.out.println("Countdown:");
 		
+		int[] num = {1,2,3,4,5,6,7,8,9,10};
 		
-		
+		for (int i = 9; i < num.length; i--) {
+			if (i == -1)
+			break;
+			System.out.println(num[i]);
+			}
 		
 		
 		
