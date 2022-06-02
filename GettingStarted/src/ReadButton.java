8f46346862b8d9bf31dd7b92e32a4e9b29fb8276
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-06-02 10:42:29 -0600

    working

--- GettingStarted/src/ReadButton.java
+++ GettingStarted/src/ReadButton.java
@@ -1,4 +1,27 @@
 
-public class ReadButton {
 
+
+import com.phidget22.*;
+
+public class ReadButton {
+    
+    public static void main(String[] args) throws Exception{
+        
+        
+        DigitalInput redButton = new DigitalInput();
+
+        
+        redButton.setHubPort(5);
+        redButton.setIsHubPortDevice(true);
+
+        
+        redButton.open(1000);
+
+        
+        while(true){
+            System.out.println("Button State: " + redButton.getState());
+            Thread.sleep(0);
+        }
+    }
 }
+  
