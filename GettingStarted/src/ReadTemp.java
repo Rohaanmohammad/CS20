fe6b2ce0a63aebe1a0a9cc7c375017928370ff17
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-06-09 10:43:57 -0600

    done

--- GettingStarted/src/ReadTemp.java
+++ GettingStarted/src/ReadTemp.java
@@ -1,4 +1,21 @@
 
-public class ReadTemp {
+ 
+import com.phidget22.*;
 
+public class ReadTemp {
+    public static void main(String[] args) throws Exception{
+
+        
+        TemperatureSensor temperatureSensor = new TemperatureSensor();
+
+        
+        temperatureSensor.open(1000);
+
+        
+        while (true) {
+            System.out.println("Temperature: " + temperatureSensor.getTemperature() + " °C" );
+            Thread.sleep(150);
+        }
+    }
 }
+  
