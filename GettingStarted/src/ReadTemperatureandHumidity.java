15e7c966d23cd9f1476da138f71b1f0df5794d36
1006003005 <1006003005@L815WD220028.edu.cbe.ab.ca>
2022-06-09 11:51:22 -0600

    working

--- GettingStarted/src/ReadTemperatureandHumidity.java
+++ GettingStarted/src/ReadTemperatureandHumidity.java
@@ -7,18 +7,24 @@ public class ReadTemperatureandHumidity {
     public static void main(String[] args) throws Exception{
 
         
-        HumiditySensor humiditySensor = new HumiditySensor();
+    	 HumiditySensor humiditySensor = new HumiditySensor();
         TemperatureSensor temperatureSensor = new TemperatureSensor();
 
         
         humiditySensor.open(1000);
         temperatureSensor.open(1000);
-
+        
        
         while(true){
             System.out.println("Humidity: " + humiditySensor.getHumidity() +" %RH, Temperature: " + temperatureSensor.getTemperature() + " °C" );
             Thread.sleep(150);
         }
+
+    
+    
+    
+    
     }
+    
 }
   
